ac5a6a2b6b952b303bb036e33cf5e47cdf1d925f
jaime <email>
2019-10-11 13:40:08 +0200

    4.4 Accion de Respuesta Rápida

--- src/app/Components/lista-correos/lista-correos.component.ts
+++ src/app/Components/lista-correos/lista-correos.component.ts
@@ -9,6 +9,9 @@ export class ListaCorreosComponent implements OnInit {
 
   correos: any[];
 
+  responder: boolean;
+  correoAResponder: any;
+
   constructor() {
     const correo1 = {
       titulo: "Titulo del 1",
@@ -29,22 +32,39 @@ export class ListaCorreosComponent implements OnInit {
       leido: false,
     };
 
-    const correo3 = {
-      titulo: "Titulo del 3",
-      cuerpo: `Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email
-        Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuer`,
-      emisor: 'correoEmisor2@openWebinar.inv',
-      destinatario: 'correoReceptor@openWebinar.inv',
-      leido: false,
-    };
-
     this.correos = [];
     this.correos.push(correo1);
     this.correos.push(correo2);
-    this.correos.push(correo3);
+    
+    this.correos.push({
+      titulo: "Titulo del 3",
+      cuerpo: `Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email
+        Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuer`,
+      emisor: 'correoEmisor3@openWebinar.inv',
+      destinatario: 'correoReceptor@openWebinar.inv',
+      leido: false,
+    });
+
+    this.correos.push({
+      titulo: "Titulo del 4",
+      cuerpo: `Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email
+        Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuer`,
+      emisor: 'correoEmisor4@openWebinar.inv',
+      destinatario: 'correoReceptor@openWebinar.inv',
+      leido: false,
+    });
+
+    this.responder = false;
   }
 
   ngOnInit() {
   }
 
+  clickResponder(correo) {
+    correo.responder = !correo.responder;
+  }
+
+  accionRespuestaRapida(correo) {
+    correo.responder = false;
+  }
 }
