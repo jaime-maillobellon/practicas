4fa6e52aec869cf3453b9af1f4683c88bdfea5e1
jaime <email>
2019-10-14 13:35:11 +0200

    4.7 Servicios

--- src/app/Components/nuevo-correo/nuevo-correo.component.ts
+++ src/app/Components/nuevo-correo/nuevo-correo.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AvisosService } from 'src/app/Services/avisos.service';
 
 @Component({
   selector: 'app-nuevo-correo',
@@ -11,12 +12,10 @@ export class NuevoCorreoComponent implements OnInit {
 
   nuevoCorreo: FormGroup;
   submitted = false;
-  
   @Input() correo: any;
-
   @Output() accionRealizada: EventEmitter<any> = new EventEmitter();
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder, private servicioAvisos: AvisosService) { }
 
   ngOnInit() {
     this.nuevoCorreo = this.formBuilder.group({
@@ -24,9 +23,9 @@ export class NuevoCorreoComponent implements OnInit {
       cuerpo: ['', [Validators.required, Validators.minLength(10)]],
       destinatario: ['', [Validators.required, Validators.email]],
     });
+   
 
     if(this.correo != undefined){
-      console.log("A",this.correo);
       this.nuevoCorreo.patchValue({
         titulo: 'Re: '+ this.correo.titulo, 
         destinatario: this.correo.emisor
@@ -35,9 +34,9 @@ export class NuevoCorreoComponent implements OnInit {
   }
 
   get formulario() { return this.nuevoCorreo.controls; }
-
   onSubmit() {
     this.submitted = true;
+
     if (this.nuevoCorreo.invalid) {
       return;
     }
@@ -46,13 +45,18 @@ export class NuevoCorreoComponent implements OnInit {
     correo.leido = false;
     correo.emisor = 'correoEmisor1@openWebinar.inv';
 
-    alert("Correo Enviado \nEliminamos el formulario");
     this.onReset();
+    this.servicioAvisos.showMenssage(`Correo enviado a ${correo.emisor}`);
   }
 
   onReset() {
     this.submitted = false;
     this.nuevoCorreo.reset();
     this.accionRealizada.emit();
+  }
+
+  cancel(){
+    this.onReset();
+    this.servicioAvisos.showMenssage("Envio Cancelado");
   }
 }
